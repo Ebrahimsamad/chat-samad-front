50685285f9ce27f0a307f25777a294b90cb301b8
Ebrahimsamad <ebrahim7asn@gmail.com>
2024-11-01 18:07:58 +0200

    edit in configurations

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-api-pearl-two.vercel.app", {
+      const socket = io("https://chat-api-production-2c0b.up.railway.app", {
         query: {
           userId: authUser._id,
         },
